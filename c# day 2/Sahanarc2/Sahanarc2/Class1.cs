0f983c2e578abdfafdda943dd171aec017901bce
Sahana Ramesh <sahanar@infinite.com>
2024-02-25 22:28:49 +0530

    My Initial Commit

---
+++ c# day 2/Sahanarc2/Sahanarc2/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sahanarc2
+{
+    class Stings_Eg
+    {
+        public static object Strings_Eg { get; private set; }
+
+        public static void String_Ops()
+        {
+            string s = "Sahana";
+            Console.WriteLine("First Time is{0}", s.GetHashCode());
+            s = "Sahana Gowda";
+            Console.WriteLine("First Time is{ 0} ", s.GetHashCode());
+
+            string s1 = "CSharp";
+            Console.WriteLine("s1 is{ 0} ", s1.GetHashCode());
+
+            string s2 = s1;
+            Console.WriteLine("s2 is{ 0} ", s2.GetHashCode());
+
+            string s3 = "Sahana";
+            Console.WriteLine("s3 refernce is as same as{ 0} ", s3.GetHashCode());
+
+            Console.WriteLine("--------Values of all String Objects------");
+            Console.WriteLine(s + " " + s1 + " " + s2 + " " + s3);
+            char[] carr = new char[] { 'w', 'e', 'l', 'c', 'o', 'm', 'e' };
+            string mystring = new string(carr); 
+            StringBuilder sb = new StringBuilder("Hello");
+            Console.WriteLine("----Original hash of string builder-------");
+            Console.WriteLine("Sbs hash code is {0} and the value is {1}", sb.GetHashCode(), sb);
+            sb.Append(", C# String Builders");
+            Console.WriteLine("----String Builder after changes----");
+            Console.WriteLine("Sbs hash code is {0} and the value is {1}", sb.GetHashCode(), sb);
+
+
+
+        }
+    }
+}
---
+++ c# day 2/sahanarc3/sahanarc3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sahanarc3
+{
+    class StaticInstance
+    {
+        static void Main(string[] args)
+        {
+            StaticInstance Si = new StaticInstance();
+            int sum = Si.AddNos(5, 6);
+            Console.WriteLine("The Sum of 2 Nos :{0}", sum);
+            EvenNos_Generation(20);
+            Console.WriteLine("===============");
+            TestClass.Method1();
+            Console.ReadKey();
+        }
+
+        private static int AddNos1(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static void EvenNos_Generation(int num)
+        {
+            int i = 0;
+            while (i <= num)
+            {
+                Console.WriteLine(i);
+                i += 2;
+            }
+        }
+
+        private int AddNos(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        class TestClass
+        {
+            public static void Method1()
+            {
+                Console.WriteLine("This is a static function of another class");
+            }
+        }
+    }
+}
+  
+
