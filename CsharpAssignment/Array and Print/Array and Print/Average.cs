a9fa7ac99901a2652849a7a85df9f3648d4da6eb
Sahana Ramesh <sahanar@infinite.com>
2024-02-26 13:09:36 +0530

    My initial commit

---
+++ CsharpAssignment/Array and Print/Array and Print/Average.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Array_and_Print
+{
+    internal class Average
+    {
+        public Average()
+        {
+        }
+
+        internal void avg()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CsharpAssignment/Day Count/Day Count/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayno;  
+
+            Console.Write("\n\n");  
+            Console.Write("Accept day number and display its equivalent day name in word:\n");  
+            Console.Write("----------------------------------------------------------------"); 
+            Console.Write("\n\n");
+
+            Console.Write("Input Day No : ");  
+            dayno = Convert.ToInt32(Console.ReadLine()); 
+
+            switch (dayno)  
+            {
+                case 1:
+                    Console.Write("Monday \n");
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Console.Write("Tuesday \n");
+                    Console.ReadKey();
+;                    break;
+                case 3:
+                    Console.Write("Wednesday \n");
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    Console.Write("Thursday \n");
+                    Console.ReadKey();
+                    break;
+                case 5:
+                    Console.Write("Friday \n");
+                    Console.ReadKey();
+                    break;
+                case 6:
+                    Console.Write("Saturday \n"); 
+                    Console.ReadKey();
+                    break;
+                case 7:
+                    Console.Write("Sunday  \n");  
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.Write("Invalid day number. \nPlease try again ....\n");
+
+                    Console.ReadKey();
+
+                    break;
+
+                    
+                   
+            }
+        }
+    }
+}
+
