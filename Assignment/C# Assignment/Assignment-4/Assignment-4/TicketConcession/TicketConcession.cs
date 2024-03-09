7e583653855fe09535dcd5b7f7554f5308e3add0
Sahana Ramesh <sahanar@infinite.com>
2024-03-09 19:41:04 +0530

    My Initial Commit

---
+++ Assignment/C# Assignment/Assignment-4/Assignment-4/TicketConcession/TicketConcession.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TicketConcession
+{
+    public class TicketConcession
+    {
+        public void CalculateConcession(int age)
+        {
+            const double TotalFare = 500.0;
+
+            if (age <= 5)
+            {
+                Console.WriteLine("Little Champs - Free Ticket");
+            }
+            else if (age > 60)
+            {
+                double discountedFare = TotalFare * 0.7; 
+                Console.WriteLine($"Senior Citizen - Fare: {discountedFare}");
+            }
+            else
+            {
+                Console.WriteLine($"Ticket Booked - Fare: {TotalFare}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
