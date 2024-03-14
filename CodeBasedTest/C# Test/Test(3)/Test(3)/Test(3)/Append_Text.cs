d6df81bafd32a1c53b2a1c457b07168b33516dbd
Sahana Ramesh <sahanar@infinite.com>
2024-03-14 10:59:18 +0530

    My Initial Commit

---
+++ CodeBasedTest/C# Test/Test(3)/Test(3)/Test(3)/Append_Text.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Test_3_
+{
+    class Append_Text
+
+    {
+        static void Main()
+        {
+            
+            string filePath = "example.txt";
+
+            try
+            {
+                
+                Console.WriteLine("Enter the text to append:");
+                string textToAppend = Console.ReadLine();
+
+               
+                if (File.Exists(filePath))
+                {
+                    
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+                       
+                        writer.WriteLine(textToAppend);
+                    }
+                }
+                else
+                {
+                    
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+
+                        writer.WriteLine(textToAppend);
+                    }
+                }
+
+                Console.WriteLine("Text appended successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ CodeBasedTest/C# Test/Test(3)/Test(3)/Test(3)/CalculatorApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_3_
+{
+    class CalculatorApp
+
+    {
+        
+        delegate int CalculatorDelegate(int x, int y);
+
+        static void Main(string[] args)
+        {
+            
+            CalculatorDelegate addition = Add;
+            CalculatorDelegate subtraction = Subtract;
+            CalculatorDelegate multiplication = Multiply;
+
+           
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine($"Addition: {addition(num1, num2)}");
+            Console.WriteLine($"Subtraction: {subtraction(num1, num2)}");
+            Console.WriteLine($"Multiplication: {multiplication(num1, num2)}");
+            Console.ReadKey();
+        }
+
+        
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
